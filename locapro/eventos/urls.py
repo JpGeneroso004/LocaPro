a7047f11aeeebb8306edd5e03363358f97dc0c4c
João Pedro Generoso <joaopedrooliveirageneroso@gmail.com>
2026-09-09 20:16:03 -0300

    Fix redundant routing prefix in eventos urls to resolve 404

--- locapro/eventos/urls.py
+++ locapro/eventos/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'eventos'
 
 urlpatterns = [
-    path('eventos/', views.EventoListView.as_view(), name='lista_eventos'),
-    path('eventos/novo/', views.EventoCreateView.as_view(), name='novo_evento'),
+    path('', views.EventoListView.as_view(), name='lista_eventos'),
+    path('novo/', views.EventoCreateView.as_view(), name='novo_evento'),
 ]
